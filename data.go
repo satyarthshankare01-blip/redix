06e8a25cd7f32b067fe14d34ef78d26f0a78254f
satyarth <satyarthshankare01@gmail.com>
2026-01-31 08:16:45 +0000

    saving data

--- data.go
+++ data.go
@@ -1,11 +1,14 @@
 package main
 
-import "os"
+import (
+	"encoding/binary"
+	"os"
+)
 
 func snapShot(store *Store) map[string]Item {
 	store.mu.Lock()
 	defer store.mu.Unlock()
-	clone := make(map[string]Item)
+	clone := make(map[string]Item , len(store.data) )
 	for key , value := range store.data{
 		clone[key] = value
 	}
@@ -13,3 +16,36 @@ func snapShot(store *Store) map[string]Item {
 	return clone 
 }
 
+
+
+func saveSnapshot(store *Store) {
+
+	copy := snapShot(store)
+	filename := "tempdata"
+
+
+	file, err  := os.Create(filename)
+	if err != nil {
+	return 
+	}
+	defer file.Close()
+    
+	for key , item := range copy {
+
+		keyBytes := []byte(key)
+		binary.Write(file , binary.LittleEndian , uint32(len(keyBytes)))
+		file.Write(keyBytes)
+
+		valueBytes := []byte(item.value)
+		binary.Write(file , binary.LittleEndian , uint32(len(valueBytes)))
+		file.Write(valueBytes)
+	}
+
+	file.Write([]byte{0xFF})
+
+	os.Rename(filename , "datadb")
+
+
+    
+
+}
